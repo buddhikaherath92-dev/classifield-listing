43bd9ef908382606a418fb7539ba4ba75f458ed3
KevinBoy98 <kavindapushpitha0@gmail.com>
2019-02-20 16:01:26 +0530

    Send message Email template updated

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -255,7 +255,7 @@
 {{--</table>--}}
 {{--</body>--}}
 {{--</html>--}}
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html style="width:100%;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0;">
 <head>
     <meta charset="UTF-8">
