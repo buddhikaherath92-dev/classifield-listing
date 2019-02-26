f577d9d25ce10f3c690b9ebac49b3937535a5d34
OshadiSL <oshadhichamodya1998@gmail.com>
2019-02-26 17:19:18 +0530

    change a blade

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,8 +17,8 @@
                     <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row" >
+                            <label for="email" class="col-md-3 col-form-label text-md-right" >{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -33,7 +33,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-success">Send Password Reset Link
+                                <button type="submit" class="btn btn-success" style="margin-left: 55px">Send Password Reset Link
                                     {{--{{ __('Send Password Reset Link') }}--}}
                                 </button>
                             </div>
