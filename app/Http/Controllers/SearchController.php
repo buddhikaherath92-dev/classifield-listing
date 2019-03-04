310a48846b02fc6916de0f158b379c7237aaedd2
punsara <PunsaraPrathibhaGPP@gmail.com>
2019-03-04 14:16:21 +0530

    Debug the displaying advertisement detail status error in search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,22 +14,30 @@ class SearchController extends Controller
     public function show()
     {
 
-        $heading = "Search result for ' ' ";
+        $heading = "Search result for ";//All Advertisements
 
         $advertisements = Advertisement::join('users', 'advertisements.user_id', 'users.id');
 
         if (request('ad_type') === 'corporate' || request('ad_type') === 'individual') {
             $advertisements = $advertisements->where('users.type',
                 config('constance.user_types')[request('ad_type')]);
+            request('district')!==null ?
+                $heading = 'Search results for All '.request('ad_type').' Advertisements In '.request('district').' District.':
+                $heading = 'Search results for All '.request('ad_type').' Advertisements';
         }
 
         if (request('district') !== null) {
             $advertisements = $advertisements->Where('district', request('district'));
+            $heading = 'Search results for '.request('ad_type').' Advertisements in ' .request('district').' District.';
         }
 
         if (request('keyword') !== null) {
             $advertisements = $advertisements->where('title', 'like', '%' . request('keyword') . '%');
-            $heading = 'Search results for ' . '" ' . request('keyword') . ' " ';
+            $heading = 'Search results for ' . request('keyword');
+        }
+
+        if(request('keyword') === null && request('district') === null && request('ad_type') === 'all'){
+            $heading = 'Search results for All Advertisements';
         }
 
         return view('web.pages.listing', [
