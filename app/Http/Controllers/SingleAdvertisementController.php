799e6459a9c4d4233b6616ea50db8fa71de79dd2
Sasanka Viraj <sasankavirajsva@gmail.com>
2019-03-04 13:56:07 +0530

    Added Changes to the master

--- app/Http/Controllers/SingleAdvertisementController.php
+++ app/Http/Controllers/SingleAdvertisementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Advertisement;
 use App\Helpers\Common;
+use App\Rating;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -34,6 +35,7 @@ class SingleAdvertisementController extends Controller
     public function show(Request $request){
 
         $advertisement = Advertisement::where('slug', $request->slug)->first();
+        $advertisement->increment('views');
         if($advertisement->is_inactive === 0){
             return view('web.pages.single_advertisement', [
                 'advertisement' => $advertisement,
@@ -41,7 +43,8 @@ class SingleAdvertisementController extends Controller
                 'category' => config('constance.categories')[$advertisement->category_id]['name'],
                 'seller' => User::where('id', $advertisement->user_id)->first(),
                 'seller_ads' => Advertisement::where('user_id', $advertisement->user_id)->where('is_inactive', 0)
-                    ->limit(4)->orderBy('created_at', 'desc')->get()
+                    ->limit(4)->orderBy('created_at', 'desc')->get(),
+                'rating'=>Rating::where('slug',$request->slug)->average('rating')
             ]);
         }else{
             $this->common->showAlerts('Your Advertisement is still inactive', 'success', "Your Advertisement is still inactive");
