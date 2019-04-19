0edd8674e7b814876ede5addb4893bd9ff552af0
OshadiSL <oshadhichamodya1998@gmail.com>
2019-04-19 17:51:15 +0530

    create a contact us page and send message admin

---
+++ app/Http/Controllers/AdminMessageController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sanju
+ * Date: 4/18/19
+ * Time: 12:06 PM
+ */
+
+namespace App\Http\Controllers;
+use App\Mail\sendAdminMessage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class AdminMessageController extends Controller{
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+      function index(Request $request){
+          $emailAddress="oshadhichamodya1998@gmail.com";
+//        $emailAddress =env('ADMIN_EMAIL',"oshadhichamodya1998@gmail.com");
+          $name=request()->all()['name'];
+          Mail::to($emailAddress)->send(new sendAdminMessage($name));
+          return view('web.pages.contact_us');
+
+
+    }
+
+}
