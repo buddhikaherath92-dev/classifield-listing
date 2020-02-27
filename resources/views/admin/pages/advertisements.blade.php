a4affa959ebd21ce165222384e9c69e28c0e21cc
Buddhika Herath <buddhikaherath.dev@gmail.com>
2020-02-27 11:10:21 +0530

    Admin panel advertisements disable past dates in expire date picker

--- resources/views/admin/pages/advertisements.blade.php
+++ resources/views/admin/pages/advertisements.blade.php
@@ -253,6 +253,21 @@
             $("#exId").val(exId);
             $("#exDate").val(exDate);
         });
+
+        $(function(){
+            var dtToday = new Date();
+
+            var month = dtToday.getMonth() + 1;
+            var day = dtToday.getDate();
+            var year = dtToday.getFullYear();
+            if(month < 10)
+                month = '0' + month.toString();
+            if(day < 10)
+                day = '0' + day.toString();
+
+            var maxDate = year + '-' + month + '-' + day;
+            $('#exDate').attr('min', maxDate);
+        });
     </script>
 
 
